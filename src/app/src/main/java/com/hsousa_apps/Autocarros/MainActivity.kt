df435cb55a1a5e5fc748a6691c235fadc14853af
sousaUA <hsousa@ua.pt>
2021-07-11 17:57:52 +0100

    navbar

--- src/app/src/main/java/com/hsousa_apps/Autocarros/MainActivity.kt
+++ src/app/src/main/java/com/hsousa_apps/Autocarros/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.hsousa_apps.Autocarros
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        try { this.supportActionBar!!.hide() } catch (e: NullPointerException) { }
         setContentView(R.layout.home)
+
+        /*nav
+        val bn = findViewById<BottomNavigationView>(R.id.bottom_navigation)!!
+        var target : Fragment
+
+        bn.setOnNavigationItemSelectedListener { item ->
+            target = HomeFragment()
+            when (item.itemId) {
+
+                //TODO: Adicionar outros items a navbar
+                R.id.home_nb -> {
+                    target = HomeFragment()
+                }
+
+                R.id.wl_nb -> {
+                    target = WishlistFragment()
+                }
+
+                R.id.set_nb -> {
+                    target = SettingsFragment()
+                }
+                else -> print("")
+            }
+
+            swapFrags(target)
+
+            true
+        }
+
+        swapFrags(HomeFragment())
+    }
+
+    private fun swapFrags(f : Fragment) {
+        val t = supportFragmentManager.beginTransaction()
+        t.replace(R.id.frag_container, f)
+        t.addToBackStack(null)
+        t.commit()
+    }
+
+    override fun onBackPressed() {
+        if ((supportFragmentManager.backStackEntryCount > 1) || (supportFragmentManager.findFragmentById(R.id.frag_container)  !is HomeFragment)) {
+            supportFragmentManager.popBackStack()
+        }
+        else {
+            finish()
+        }*/
     }
 }
