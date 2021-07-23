8f52c5048b76199f212c4d440629eb88dcac10c6
sousaUA <hsousa@ua.pt>
2021-07-23 14:41:57 +0100

    varela bus schedule source

--- src/app/src/main/java/com/hsousa_apps/Autocarros/data/Location.kt
+++
@@ -1,4 +0,0 @@
-package com.hsousa_apps.Autocarros.data
-
-class Location constructor(val busStop: String, val coordinates: String) {
-}
--- src/app/src/main/java/com/hsousa_apps/Autocarros/data/Time.kt
+++
@@ -1,4 +0,0 @@
-package com.hsousa_apps.Autocarros.data
-
-class Time constructor(val hour: String, val type: TypeOfDay) {
-}
