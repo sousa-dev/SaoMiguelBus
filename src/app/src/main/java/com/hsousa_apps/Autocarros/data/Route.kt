854ee44b3e12898fb3acf8147064af973aa30fcd
sousaUA <hsousa@ua.pt>
2021-07-25 13:10:03 +0000

    kt classes "redesign"

--- src/app/src/main/java/com/hsousa_apps/Autocarros/data/Route.kt
+++ src/app/src/main/java/com/hsousa_apps/Autocarros/data/Route.kt
@@ -1,6 +1,6 @@
 package com.hsousa_apps.Autocarros.data
 
-class Route constructor(val id: String, val stops: Map<Stop, String>, val day: TypeOfDay) {
+class Route constructor(val id: String, val stops: Map<Stop, List<String>>, val day: TypeOfDay, val company: Int) {
     private var allStops: List<Stop> = stops.keys as List<Stop>
     private var origin: Stop? = allStops[0]
     private var destination: Stop? = allStops[allStops.size - 1]
