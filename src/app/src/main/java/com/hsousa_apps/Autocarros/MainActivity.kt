bdc50b84537f5fcdcdf8a72cac1a56b3161c4a0c
Sousa Dev <hsousa@ua.pt>
2022-07-30 19:57:08 +0100

    GET response from API

--- src/app/src/main/java/com/hsousa_apps/Autocarros/MainActivity.kt
+++ src/app/src/main/java/com/hsousa_apps/Autocarros/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.hsousa_apps.Autocarros
 
+import android.app.VoiceInteractor
 import android.content.SharedPreferences
 import org.osmdroid.config.Configuration.*
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
@@ -25,20 +33,36 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        val URL = "https://saomiguelbus-api.herokuapp.com/api/v1/android/load"
         loadData()
         super.onCreate(savedInstanceState)
         try { this.supportActionBar!!.hide() } catch (e: NullPointerException) { }
         setContentView(R.layout.main)
 
         Datasource().changeCurrentLang(Locale.getDefault().language)
-        if (!Datasource().getLoaded()) Datasource().load()
+
+        /** Fetch routes from API **/
+        val requestQueue: RequestQueue = Volley.newRequestQueue(this)
+        val objectRequest: JsonArrayRequest = JsonArrayRequest(
+            Request.Method.GET,
+            URL,
+            null,
+            Response.Listener { response ->
+                Log.d("RESPONSE", response.toString())
+                /**TODO: populate static files with response routes**/
+            },
+            Response.ErrorListener { error ->
+                Log.d("FAILED RESPONSE", error.toString())
+                if (!Datasource().getLoaded()) Datasource().load()
+            }
+        )
+        requestQueue.add(objectRequest)
 
         if (Locale.getDefault().language != "pt"){
             Functions().translateStops(Locale.getDefault().language)
         }
 
         getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
-
 
         Datasource().loaded()
 
