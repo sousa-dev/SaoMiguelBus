636e2f6753161e71a1478893f2def20d85add58b
sousaUA <hsousa@ua.pt>
2023-03-19 14:22:52 +0000

    Add StepModel class

--- src/app/src/main/java/com/hsousa_apps/Autocarros/models/StepModel.kt
+++ src/app/src/main/java/com/hsousa_apps/Autocarros/models/StepModel.kt
@@ -1,4 +1,32 @@
 package com.hsousa_apps.Autocarros.models
 
 class StepModel {
+    var id: String? = null
+    var icon: Int? = null
+    var action: String? = null
+    var goal: String? = null
+    var distance : Int? = null
+    var time : String? = null
+
+    constructor(
+        id: String?,
+        icon: Int?,
+        action: String?,
+        goal: String?,
+        distance: Int?,
+        time: String?
+    ) {
+        this.id = id
+        this.icon = icon
+        this.action = action
+        this.goal = goal
+        this.distance = distance
+        this.time = time
+    }
+
+    override fun toString(): String {
+        return "StepModel(id=$id, icon=$icon, action=$action, goal=$goal, distance=$distance, time=$time)"
+    }
+
+
 }
