2fd226204529d2593fe5a8e12571f091541d801f
sousaUA <hsousa@ua.pt>
2023-07-06 16:13:26 +0000

    create utils

---
+++ src/saomiguelbus/lib/utils/index.dart
@@ -0,0 +1,1 @@
+export 'on_change.dart';
---
+++ src/saomiguelbus/lib/utils/on_change.dart
@@ -0,0 +1,2 @@
+void onChangeOriginHome(homeOrigin) => print("homeOrigin changed to $homeOrigin");
+void onChangeDestinationHome(homeDestination) => print("homeDestination changed to $homeDestination");
