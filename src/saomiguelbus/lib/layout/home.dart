5cbd2fc46909a7c9ee86c9c2016ae4fb921cd110
sousaUA <hsousa@ua.pt>
2023-08-04 11:25:35 +0000

    Autocomplete for the Origin on the Home Page

--- src/saomiguelbus/lib/layout/home.dart
+++ src/saomiguelbus/lib/layout/home.dart
@@ -1,11 +1,11 @@
 // Home Page Body Widget
 // Path: lib/layout/home.dart
-
 import 'package:flutter/material.dart';
 
 import 'package:saomiguelbus/models/type_of_day.dart';
 import 'package:saomiguelbus/services/index.dart';
 import 'package:saomiguelbus/models/globals.dart';
+import 'package:saomiguelbus/utils/remove_diacritics.dart';
 
 class HomePageBody extends StatefulWidget {
   HomePageBody(
@@ -31,20 +31,38 @@ class _HomePageBodyState extends State<HomePageBody> {
       padding: const EdgeInsets.all(16.0),
       child: Column(
         children: [
-          TextField(
-            decoration: const InputDecoration(
-              labelText: 'Origin',
-              border: OutlineInputBorder(),
-            ),
-            onChanged: (value) {
-              widget.onChangeOrigin(value);
+          Autocomplete<String>(
+            optionsBuilder: (TextEditingValue textEditingValue) {
+              if (textEditingValue.text == '') {
+                return const Iterable<String>.empty();
+              }
+              List<String> _stopMatches = <String>[];
+              _stopMatches.addAll(allStops.keys.cast<String>());
+
+              _stopMatches.retainWhere((stop) {
+                return removeDiacritics(stop.toLowerCase()).contains(
+                    removeDiacritics(textEditingValue.text.toLowerCase()));
+              });
+              return _stopMatches;
             },
-            // (value) {
-            //   setState(() {
-            //     _origin = value;
-            //   });
-            // },
+            onSelected: (String selection) {
+              widget.onChangeDestination(selection);
+            },
           ),
+          // TextField(
+          //   decoration: const InputDecoration(
+          //     labelText: 'Origin',
+          //     border: OutlineInputBorder(),
+          //   ),
+          //   onChanged: (value) {
+          //     widget.onChangeOrigin(value);
+          //   },
+          //   // (value) {
+          //   //   setState(() {
+          //   //     _origin = value;
+          //   //   });
+          //   // },
+          // ),
           const SizedBox(height: 16.0),
           TextField(
             decoration: const InputDecoration(
