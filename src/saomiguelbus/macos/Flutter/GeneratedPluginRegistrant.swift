b9f2ae7ce7f2ef1248bba11a1b6e6bc09be9a6c8
sousaUA <hsousa@ua.pt>
2023-08-10 15:24:56 +0000

    macOS update commit

--- src/saomiguelbus/macos/Flutter/GeneratedPluginRegistrant.swift
+++ src/saomiguelbus/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import package_info
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
