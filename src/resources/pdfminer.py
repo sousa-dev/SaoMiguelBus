88030301ef07808de3b5fc27bd151b8f34df7fab
sousaUA <hsousa@ua.pt>
2023-08-21 11:41:00 +0000

    Add more detail in output

--- src/resources/pdfminer.py
+++ src/resources/pdfminer.py
@@ -1,4 +1,4 @@
 import easyocr
 reader = easyocr.Reader(['pt']) 
-result = reader.readtext('doc/schedule/avm/fromFB/Horários Carreiras Noturnas Capelas (Segunda a Domingo).jpg', detail = 0)
+result = reader.readtext('doc/schedule/avm/fromFB/Horários Carreiras Noturnas Capelas (Segunda a Domingo).jpg')
 print(result)
