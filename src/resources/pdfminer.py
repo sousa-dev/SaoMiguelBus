234b430a2cd0cfeaa58e19913453d3767b92d590
sousaUA <hsousa@ua.pt>
2023-08-21 11:08:45 +0000

    macOS update

--- src/resources/pdfminer.py
+++ src/resources/pdfminer.py
@@ -1,19 +1,4 @@
-from PIL import Image
-from pytesseract import pytesseract
-  
-# Defining paths to tesseract.exe 
-# and the image we would be using
-image_path = "doc/schedule/avm/sunday.jpg"
-  
-# Opening the image & storing it in an image object
-img = Image.open(image_path)
-  
-  
-# Passing the image object to 
-# image_to_string() function
-# This function will
-# extract the text from the image
-text = pytesseract.image_to_string(img)
-  
-# Displaying the extracted text
-print(text[:-1])
+import easyocr
+reader = easyocr.Reader(['pt']) 
+result = reader.readtext('doc/schedule/avm/fromFB/Horários Carreiras Noturnas Capelas (Segunda a Domingo).jpg', detail = 0)
+print(result)
