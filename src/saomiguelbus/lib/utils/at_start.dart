1f3949eb7eaf4cf6163262b58c3ed50d2d014a29
sousaUA <hsousa@ua.pt>
2023-09-19 16:02:22 +0100

    Generate session token

--- src/saomiguelbus/lib/utils/at_start.dart
+++ src/saomiguelbus/lib/utils/at_start.dart
@@ -1,8 +1,12 @@
 import 'dart:developer' as developer;
+import 'package:uuid/uuid.dart';
 
 import 'package:saomiguelbus/services/index.dart';
+import 'package:saomiguelbus/models/globals.dart';
 
 bool start(kDebugMode) {
+  sessionToken = const Uuid().v4();
+
   retrieveData(kDebugMode);
   kDebugMode ? init_debug_mode() : init_release_mode();
   return true;
