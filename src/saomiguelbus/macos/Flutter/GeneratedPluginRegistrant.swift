65b535247225a34f09547b990408b6ea222c340f
sousaUA <hsousa@ua.pt>
2023-09-26 10:09:51 +0100

    Update build files

--- src/saomiguelbus/macos/Flutter/GeneratedPluginRegistrant.swift
+++ src/saomiguelbus/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import package_info
 import shared_preferences_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
